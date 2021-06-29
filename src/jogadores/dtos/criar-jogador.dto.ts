9ce1bfe4ce5bbb12e1501d3901b9202f33da305a
felipeduarte <felipesilvapac@gmail.com>
2021-06-29 01:01:02 -0300

    iniciando interface do jogador

---
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -0,0 +1,5 @@
+export class criarJogadorDto {
+  readonly telefoneCelular: string;
+  readonly email: string;
+  readonly nome: string;
+}
--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,14 @@
+import { Body } from '@nestjs/common';
 import { Controller, Post } from '@nestjs/common';
+import { criarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogador() {
-    return JSON.stringify({
-      nome: 'Felipe',
-    });
+  async criarAtualizarJogador(@Body() criarJogadorDto: criarJogadorDto) {
+    const { email } = criarJogadorDto;
+    return JSON.stringify(`{
+      "email": ${email},
+    }`);
   }
 }
